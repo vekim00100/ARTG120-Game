0a5602f55529bf8c22322aa404438bc17871f40e
vekim00100 <vekim@ucsc.edu>
2025-03-19 03:57:33 -0700

    Implemented Press ESC to exit

--- Scripts/Characters/player (2).gd
+++ Scripts/Characters/player (2).gd
@@ -36,6 +36,9 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
